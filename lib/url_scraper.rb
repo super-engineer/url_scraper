367458faed7ca8cf1f59e84d7bc8d41b173cdb5e
Administrator (Varun) <varun@genii.in>
2013-07-23 22:19:03 +0530

    now images are returned with absolute urls, always

--- lib/url_scraper.rb
+++ lib/url_scraper.rb
@@ -8,13 +8,13 @@ require 'thor'
 module UrlScraper
   # Tell rails to load all assets
   class Engine < Rails::Engine
-  
+
   end
 
   class CLI < Thor
 
   end
-  
+
   # Handles the url request
 
   # Fetch Open Graph data from the specified URI. Makes an
@@ -23,14 +23,14 @@ module UrlScraper
   #
   # Pass <tt>false</tt> for the second argument if you want to
   # see invalid (i.e. missing a required attribute) data.
-  
-  def self.fetch(uri, strict = true)  
-    parse(RestClient.get(uri).body, strict)
+
+  def self.fetch(uri, strict = true)
+    parse(RestClient.get(uri).body, strict, uri)
     rescue RestClient::Exception, SocketError
       false
   end
-  
-  def self.parse(html, strict = true)
+
+  def self.parse(html, strict = true, uri)
     logger = Logger.new(STDOUT)
     doc = Nokogiri::HTML.parse(html)
     page = UrlScraper::Object.new
@@ -40,12 +40,12 @@ module UrlScraper
       end
     end
 
-    page.title = (doc.at_css('title').text rescue nil) if page.title.nil? 
+    page.title = (doc.at_css('title').text rescue nil) if page.title.nil?
     if page.description.nil?
       page.description = doc.at_css("meta[name='description']")['content'] unless doc.at_css("meta[name='description']").nil?
     end
     if page.image.nil?
-      image_array = doc.css("img").take(3).collect{|img| img['src']}
+      image_array = doc.css("img").take(5).collect{|img| URI.parse(uri).merge(URI.parse img['src'].to_s).to_s }
       page.image = image_array unless image_array.empty?
     end
     # return false if page.keys.empty?
@@ -53,7 +53,7 @@ module UrlScraper
     page
     # return doc
   end
-  
+
   TYPES = {
     'activity' => %w(activity sport),
     'business' => %w(bar company cafe hotel restaurant),
@@ -64,38 +64,38 @@ module UrlScraper
     'product' => %w(album book drink food game movie product song tv_show),
     'website' => %w(blog website)
   }
-  
+
   # The UrlScraper::Object is a Hash with method accessors for
   # all detected Open Graph attributes.
   class Object < Hashie::Mash
     MANDATORY_ATTRIBUTES = %w(title type image url)
-    
+
     # The object type.
     def type
       self['type']
     end
-    
+
     # The schema under which this particular object lies. May be any of
     # the keys of the TYPES constant.
     def schema
-      UrlScraper::TYPES.each_pair do |schema, types| 
+      UrlScraper::TYPES.each_pair do |schema, types|
         return schema if types.include?(self.type)
       end
       nil
     end
-    
+
     UrlScraper::TYPES.values.flatten.each do |type|
       define_method "#{type}?" do
         self.type == type
       end
     end
-    
+
     UrlScraper::TYPES.keys.each do |scheme|
       define_method "#{scheme}?" do
         self.type == scheme || UrlScraper::TYPES[scheme].include?(self.type)
       end
     end
-    
+
     # If the Open Graph information for this object doesn't contain
     # the mandatory attributes, this will be <tt>false</tt>.
     def valid?
